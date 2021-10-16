42a10090367098fa0da7313031543717caa25d4f
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-16 19:37:57 -0400

    Added MeltyBot to docker images

--- BotControlPanel.App/Constants/DockerImages.cs
+++ BotControlPanel.App/Constants/DockerImages.cs
@@ -5,5 +5,6 @@
         public const string DocBot = "sgarberg/docbot";
         public const string DogBot = "sgarberg/dogbot";
         public const string ColorBot = "sgarberg/colorbot";
+        public const string MeltyBot = "chrisplummer/meltybot";
     }
 }
