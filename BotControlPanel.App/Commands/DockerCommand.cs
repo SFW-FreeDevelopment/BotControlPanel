c0b7858aebf89b202f5ad117df6da273d09e97f8
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-16 17:05:02 -0400

    Added static commands

---
+++ BotControlPanel.App/Commands/DockerCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace BotControlPanel.App.Commands
+{
+    public static class DockerCommand
+    {
+        public static string Stats()
+        {
+            return "sudo docker stats";
+        }
+        
+        public static string Run(string imageName)
+        {
+            return $"sudo docker run {imageName}";
+        }
+        
+        public static string Pull(string imageName)
+        {
+            return $"sudo docker pull {imageName}";
+        }
+        
+        public static string Kill(string containerName)
+        {
+            return $"sudo docker run {containerName}";
+        }
+    }
+}
