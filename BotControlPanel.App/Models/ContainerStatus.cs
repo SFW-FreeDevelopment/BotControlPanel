380099d004aa740e544c27b12d14bb8e2f815e89
Steven Garberg <steven.garberg.jr@gmail.com>
2021-10-17 13:27:50 -0400

    Added container status class

---
+++ BotControlPanel.App/Models/ContainerStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace BotControlPanel.App.Models
+{
+    public class ContainerStatus
+    {
+        public string ContainerId { get; set; }
+        public string Image { get; set; }
+        public string Command { get; set; }
+        public string Created { get; set; }
+        public string Status { get; set; }
+        public string Ports { get; set; }
+        public string Names { get; set; }
+    }
+}
